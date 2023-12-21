a2e1569db3a8ac5b0326ec9ad19991b75d0292b0
Your Name <you@example.com>
2023-12-21 19:25:06 +0500

    zor masala

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char[] chars = {'c', 'h', 'a', 'r'};
+
+        for (char c : chars) {
+            int intRepresentation =  c;
+          /*  System.out.println("'"+c+"'Expected to find exactly one "+"'"+c+"' "+"char representation");*/
+            System.out.println(c);
+        }
     }
 }
